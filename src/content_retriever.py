9a3e1f419a29f839c027a61031db8e37e2347966
alex <you@example.com>
2024-06-05 10:36:33 +0300

    добавил многопоточность

--- src/content_retriever.py
+++ src/content_retriever.py
@@ -186,6 +186,67 @@ def get_source_links(product_source_links: List[SourceLink]) -> List[TextInfoFro
     
     return results
 
+def get_source_links_single(source_link: SourceLink) -> TextInfoFromSource:
+
+    # Extract the link from the current source
+    link = source_link['link']
+    print(link)
+
+    try:
+        if link.lower().endswith('.pdf'):
+            # Handle link as a PDF directly
+            pdf_url = link
+            pdf_path = download_file(pdf_url, 'downloads', 30)
+            pdf_text = extract_text_from_pdf(pdf_path)
+            text_info = TextInfoFromSource(
+              html_text=None,  # No HTML content for a direct PDF link
+              pdf_texts=[pdf_text],
+              source=source_link
+            )
+
+        else:
+            # Make an HTTP request to the specified link
+            response = requests.get(link)
+            # Create a BeautifulSoup object to parse the HTML content of the page
+            soup = BeautifulSoup(response.content, 'html.parser')
+            
+            # Extract all text from the HTML, using newline as separator and stripping extra spaces
+            html_text = soup.get_text(separator="\n", strip=True)
+            
+            # Find and download PDFs
+            pdf_texts = [] # to store texts from PDF files
+            for a_tag in soup.find_all('a', href=True):
+                # Extract the href value from the <a> tag
+                href = a_tag['href']
+                # Check if the link ends with .pdf (i.e., it's a PDF file)
+                if href.lower().endswith('.pdf'):
+                    # Form the complete URL for the PDF file
+                    pdf_url = href if href.startswith('http') else link + href
+                    # Download the PDF file and save it to the specified directory
+                    pdf_path = download_file(pdf_url, 'downloads', 30)
+                    # Extract text from the downloaded PDF file
+                    pdf_text = extract_text_from_pdf(pdf_path)
+                    pdf_texts.append(pdf_text)
+            
+            # Create a dictionary with information about the HTML and PDF texts
+            text_info = TextInfoFromSource(
+                html_text=html_text,
+                pdf_texts=pdf_texts if pdf_texts else None,
+                source=source_link
+            )
+    except requests.exceptions.RequestException as e:
+        print(f"Error downloading content from {link}: {e}")
+        # Создать пустой объект TextInfoFromSource с сообщением об ошибке
+        text_info = TextInfoFromSource(
+            html_text=f"Error downloading content: {e}",
+            pdf_texts=None,
+            source=source_link
+        )
+
+    
+    return text_info
+
+
 def save_to_csv(texts, filename="extracted_texts.csv"):
     """
     Saves the list of TextInfoFromSource objects (`texts`) to a CSV file.
@@ -256,89 +317,28 @@ def read_links_from_csv(file_path: str, max_rows: int = None) -> List[SourceLink
 if __name__ == "__main__":
     # print("test")
 
-    # # Пример использования функции
-    # product_source_links = [
-    #     {"link": "https://consumer.huawei.com/kz/phones/y5p/specs/", "confidence_rate": 0.9},
-    #     # {"link": "https://static.digma.ru/data/download/manuals/EVE-14-C414-ID-1795672_manual_2022-08-03.pdf", "confidence_rate": 0.9},
-    #     # {"link": "https://ru.msi.com/Monitor/MAG-274QRF-QD-E2", "confidence_rate": 0.8},
-    #     {"link": "https://www.mi.com/ru/product/redmi-note-10t/specs", "confidence_rate": 0.8},
-    #     # {"link": "https://example.org", "confidence_rate": 0.8},
-    #     # {"link": "https://example.org", "confidence_rate": 0.8},
-    #     # {"link": "https://example.org", "confidence_rate": 0.8},
-    # ]
-
-
-    # texts = get_source_links(product_source_links)
-
-    # import sys
-    # # Изменение кодировки стандартного вывода на utf-8
-    # sys.stdout.reconfigure(encoding='utf-8')
-    
-    # # print("test2")
-    # print(texts)
-
-    # # for text_info in texts:
-    # #     # print(text_info.pdf_texts)
-    # #     print("------------------")
-    # #     if text_info["pdf_texts"]:
-    # #         for pdf_text in text_info["pdf_texts"]:
-    # #             print(pdf_text)
-
-    
-
-    # def save_to_csv(texts, filename="extracted_texts.csv"):
-    #     """
-    #     Saves the list of TextInfoFromSource objects (`texts`) to a CSV file.
-
-    #     Args:
-    #       texts (List[TextInfoFromSource]): The list of text information objects to save.
-    #       filename (str, optional): The name of the CSV file to create. Defaults to "extracted_texts.csv".
-    #     """
-
-    #     # Check if the file exists
-    #     if os.path.exists(filename):
-    #         # Delete the existing file to overwrite it
-    #         os.remove(filename)
-
-    #     with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
-    #         fieldnames = ['link', 'confidence_rate', 'html_text', 'pdf_texts']
-    #         # writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    #         writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=";")
-
-    #         # Write the header row
-    #         writer.writeheader()
-
-    #         for text_info in texts:
-    #             # Обработка возможного отсутствия ключа 'pdf_texts'
-    #             pdf_texts = text_info.get('pdf_texts', [])  # Пустой список по умолчанию
-    #             if pdf_texts is None:
-    #                 pdf_texts = []  # Обеспечить наличие списка, даже если 'pdf_texts' отсутствует
-
-    #             # Объединение элементов 'pdf_texts' в строку с разделителем '\n'
-    #             pdf_texts_joined = ",".join(pdf_texts)
-
-
-    #             # Create a dictionary with the data to write
-    #             data = {
-    #               'link': text_info['source']['link'],
-    #               'confidence_rate': text_info['source']['confidence_rate'],
-    #               'html_text': text_info['html_text'],
-    #               'pdf_texts': pdf_texts_joined
-    #             }
-
-    #             # Write the data row
-    #             writer.writerow(data)
-
-    # # Example usage (assuming `texts` is already populated)
-    # save_to_csv(texts)
-
-
-
 
     input_csv_path = "Links_cleaned.csv"
     # product_source_links = read_links_from_csv(input_csv_path)
-    product_source_links = read_links_from_csv(input_csv_path, max_rows=50)
-    texts = get_source_links(product_source_links)
-    save_to_csv(texts)
-    print("Finished processing and saving extracted texts to CSV.")
+    product_source_links = read_links_from_csv(input_csv_path, max_rows=2000)
+
+
+    # texts = get_source_links(product_source_links)
+    # save_to_csv(texts)
+    # print("Finished processing and saving extracted texts to CSV.")
+
+
+    import concurrent.futures
+
+    # Use ThreadPoolExecutor for multithreaded processing
+    with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+        results = executor.map(get_source_links_single, product_source_links)
+
+    # Process the results
+    text_info_from_sources = []
+    for result in results:
+        if result is not None:
+            text_info_from_sources.append(result)
     
+    save_to_csv(text_info_from_sources)
+    print("Finished processing and saving extracted texts to CSV.")
